eb819e43be6743e2f2bcd458b287266dd088bd4d
Fernando Serpa <serpaef@outlook.com>
2022-03-25 12:40:01 -0300

    tipos primitivos: ok

--- src/App.java
+++ src/App.java
@@ -2,5 +2,44 @@
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		/*
+		 * BEGINNING OF PRIMITIVE TYPES
+		 * */
+		
+		// 32 bit value
+		int myIntNumber = 88;
+		
+		// 16 bit value
+		short myShortNumber = 870;
+		
+		// 64 bit value
+		long myLongNumber = 8600;
+		
+		// floating point value
+		double myDoubleNumber = 8.8;
+		
+		// char value uses single quote
+		char myChar = 'f';
+		
+		// boolean
+		boolean myBool = true;
+		
+		// byte type can hold a value between -128 and 127
+		byte myByte = 127;
+		
+		
+		System.out.println(myIntNumber);
+		System.out.println(myShortNumber);
+		System.out.println(myLongNumber);
+		System.out.println(myDoubleNumber);
+		System.out.println(myChar);
+		System.out.println(myBool);
+		System.out.println(myByte);
+		
+		/*
+		 * END OF PRIMITIVE TYPES
+		 * */
+		
 	}
 }
